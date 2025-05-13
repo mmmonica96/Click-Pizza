77a276dbb15b5a6d74e4088e995ed67d426a64d4
gilvazquezcarmendelrocio@gmail.com <gilvazquezcarmendelrocio@gmail.com>
2025-05-13 09:54:51 +0200

    estructura del proyecto

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import '../css/init.css';
+import Navbar from '../components/Navbar';
+import Banner from '../components/Banner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Banner/>
+        <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ frontend/src/js/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../css/Banner.css';
+
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <img src="/banner.png" alt="Banner Click&Pizza" />
+    </div>
+  );
+}
