27fe6605fdbd40fc98e25dbe566d3077be37908e
gilvazquezcarmendelrocio@gmail.com <gilvazquezcarmendelrocio@gmail.com>
2025-05-13 21:07:56 +0200

    Diseñando Pagina Principal

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -3,14 +3,21 @@ import '../css/App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Banner />
+       <div className="logo-banner-container">
+          <img src="/img/logo.png" className="App-logo" alt="logo" />
+          <Banner />
+        </div>
         <Navbar />
-        <img src="/img/logo.png" className="App-logo" alt="logo" />
       </header>
+      <main className="App-main" >        
+  <img src="/img/Ofertas.png" className="Banner-ofertas" alt="ofertas" />
+</main>
+
     </div>
   );
 }
