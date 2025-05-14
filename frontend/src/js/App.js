f2f8e4d7dbaaf56a1b2ddc50846d82abfcd8bace
Maria <81964773+Mariagv00@users.noreply.github.com>
2025-05-14 11:37:51 +0200

    app.js modificado

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,15 +1,23 @@
 // src/js/App.js
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import '../css/App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import PizzaApp from './pizza/pizza';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <main className="App-main">
-      <img src="/img/Ofertas.png" className="Banner-ofertas" alt="ofertas" />
+      <img
+        src="/img/Ofertas.png"
+        className="Banner-ofertas"
+        alt="ofertas"
+        onClick={() => navigate("/pizza")}
+        style={{ cursor: "pointer" }}
+      />
     </main>
   );
 }
