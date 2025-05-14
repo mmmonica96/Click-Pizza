c7c6db76d3d66a9abfdf3852bf6b270a508ace39
Monica <mmarort302@g.educaand.es>
2025-05-14 09:22:17 +0200

    integracion de carrito en la barra de navegacion

--- frontend/src/js/components/Navbar.js
+++ frontend/src/js/components/Navbar.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import '../../css/Navbar.css';
+import React, { useState } from "react";
+import "../../css/Navbar.css";
 
 export default function Navbar() {
   const [menuActive, setMenuActive] = useState(false);
   const [userMenuOpen, setUserMenuOpen] = useState(false);
 
-  const toggleMenu = () => setMenuActive(prev => !prev);
-  const toggleUserMenu = () => setUserMenuOpen(prev => !prev);
+  const toggleMenu = () => setMenuActive((prev) => !prev);
+  const toggleUserMenu = () => setUserMenuOpen((prev) => !prev);
 
   return (
-    <nav className={`navbar ${menuActive ? 'active' : ''}`}>
+    <nav className={`navbar ${menuActive ? "active" : ""}`}>
       <div className="navbar-container">
         {/* Hamburger icon */}
         <div className="menu-toggle" onClick={toggleMenu}>
@@ -17,23 +17,41 @@ export default function Navbar() {
         </div>
 
         {/* Navigation menu */}
-        <ul className={`navbar-list ${menuActive ? 'active' : ''}`}>
-          <li><a href="#">Menús</a></li>
-          <li><a href="#">Pizza</a></li>
-          <li><a href="#">Entrantes</a></li>
-          <li><a href="#">Pasta</a></li>
-          <li><a href="#">Postres</a></li>
-          <li><a href="#">Contacto</a></li>
+        <ul className={`navbar-list ${menuActive ? "active" : ""}`}>
+          <li>
+            <a href="#">Menús</a>
+          </li>
+          <li>
+            <a href="#">Pizza</a>
+          </li>
+          <li>
+            <a href="#">Entrantes</a>
+          </li>
+          <li>
+            <a href="#">Pasta</a>
+          </li>
+          <li>
+            <a href="#">Postres</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
         </ul>
 
-        {/* User Icon */}
         <div className="user-menu-container">
+          {/* cart */}
+          <div className="cart-container">
+            <img src="/img/carrito.png" className="icono-cart" alt="cart" />
+          </div>
+
+          {/* account */}
           <img
             src="/img/icon-user.png"
             className="icono-user"
-            alt="icono"
+            alt="icon"
             onClick={toggleUserMenu}
           />
+
           {userMenuOpen && (
             <div className="user-dropdown">
               <a href="#">Registrarse</a>
