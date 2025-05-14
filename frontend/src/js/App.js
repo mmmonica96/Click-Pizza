cc11c14d2567838a65bfaa8d879160eca8818ef5
Monica <mmarort302@g.educaand.es>
2025-05-14 11:46:00 +0200

    cambiando nombres a minusculas

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,10 +1,11 @@
 // src/js/App.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import '../css/App.css';
-import Navbar from './components/Navbar';
-import Banner from './components/Banner';
-import PizzaApp from './pizza/pizza';
+import "../css/app.css";
+import Navbar from "./components/navbar";
+import Banner from "./components/banner";
+import PizzaApp from "./pizza/pizza";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -19,7 +20,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="logo-banner-container">
-          <img src="/img/logo.png" className="App-logo" alt="logo" />
+          <Link to="/pizza/pizza">
+            <img src="/img/logo.png" className="App-logo" alt="logo" />
+          </Link>
           <Banner />
         </div>
         <Navbar />
@@ -27,7 +30,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pizza" element={<PizzaApp />} />
+        <Route path="/pizza/pizza" element={<PizzaApp />} />
       </Routes>
     </div>
   );
--- frontend/src/js/components/Navbar.js
+++ frontend/src/js/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../../css/Navbar.css";
+import "../../css/navbar.css";
 
 export default function Navbar() {
   const [menuActive, setMenuActive] = useState(false);
