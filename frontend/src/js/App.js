f9ef64098adb46b40d3e8b32b7397d0f23b61e42
Monica <mmarort302@g.educaand.es>
2025-05-16 12:26:29 +0200

    finalizado registro

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -12,6 +12,7 @@ import PastaApp from "./comidas/pasta";
 import PostreApp from "./comidas/postres";
 import Home from "./init";
 import Contact from "./contacto";
+import Login from "./login";
 function App() {
   return (
     <div className="App">
@@ -32,6 +33,7 @@ function App() {
         <Route path="/pasta" element={<PastaApp />} />
         <Route path="/postres" element={<PostreApp />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/contacto" element={<Contact />} />
       </Routes>
 
