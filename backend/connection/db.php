cc92b38af5c313d37931f2a743c00c9d86c4c575
Monica <mmarort302@g.educaand.es>
2025-05-16 08:42:01 +0200

    conexion a la base de datos

---
+++ backend/connection/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "clickpizza";
+
+//create connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+//verify connection
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
