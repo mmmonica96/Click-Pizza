a5b5c6bf15281b5f9d153af8a33fc37507e141a2
gilvazquezcarmendelrocio@gmail.com <gilvazquezcarmendelrocio@gmail.com>
2025-05-19 19:42:22 +0200

    Arreglos pagina menu

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -15,6 +15,7 @@ import Login from "./login";
 import Contact from "./contacto";
 import CartModal from "./components/cartModal";
 import PizzaBuilder from "./create-pizza/create-pizza";
+import Menus from "./main";
 
 function App() {
   return (
@@ -41,6 +42,7 @@ function App() {
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/cart" element={<CartModal />} />
         <Route path="/crear-pizza" element={<PizzaBuilder />} />
+        <Route path="/menus" element={<Menus />} />
 
       </Routes>
 
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../css/main.css';
+import '../css/main.css';
 
 export default function Menus() {
   return (
     <div className="menus-container">
       {/* Left Sidebar */}
       <div className="sidebar">
-        <div className="logo-container">
-          <img src="/img/logo.png" alt="Click & Pizza" className="logo" />
-          <h3>CLICK & PIZZA</h3>
-        </div>
         <div className="menu-categories">
           <Link to="/pizzas">Pizzas</Link>
           <Link to="/pasta">Pasta</Link>
@@ -21,37 +17,21 @@ export default function Menus() {
 
       {/* Main Content */}
       <div className="main-content">
-        {/* Top Bar: Navigation + Cart */}
-        <div className="topbar">
-          <div className="topbar-title">Menus</div>
-          <div className="topbar-menu">
-            <ul>
-              <li><Link to="#">Menus</Link></li>
-              <li><Link to="/pizzas">Pizzas</Link></li>
-              <li><Link to="#">Appetizers</Link></li>
-              <li><Link to="#">Pasta</Link></li>
-              <li><Link to="#">Desserts</Link></li>
-              <li><Link to="#">Contact</Link></li>
-            </ul>
-            <div className="cart">
-              <img src="/img/carrito.png" alt="Cart" />
-              <button>Cart</button>
-            </div>
-          </div>
+       
         </div>
 
         {/* Combo Cards */}
         <div className="combos-container">
           <div className="combo-card">
-            <p>Pizza Combo1</p>
-            <button>Order</button>
+            <p>elTriple</p>
+            <button>Pedir</button>
           </div>
           <div className="combo-card">
-            <p>Pizza Combo2</p>
-            <button>Order</button>
+            <p>Family days</p>
+            <button>Pedir</button>
           </div>
         </div>
       </div>
-    </div>
+    
   );
 }
